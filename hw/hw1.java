61e5454f2fb4def775108bd382900e339961c4f9
Borui Wang <Borui@BoruideMacBook-Air.local>
2015-10-11 15:23:30 -0700

    update hw1

--- hw/hw1.java
+++ hw/hw1.java
@@ -1,10 +1,10 @@
 public class hw1 {
-	
-	public static void main(String[] args) {
-		int SIZE = 5;
+
+	// exercise 1
+	public static void DrawTriangle(int N) {
 		int row = 1;
 
-		while(row <= SIZE) {
+		while(row <= N) {
 			int col = 0;
 			while(col < row) {
 				System.out.print("*");
@@ -13,5 +13,24 @@ public class hw1 {
 			row += 1;
 			System.out.println();
 		}
+	}
+
+	// exercise 2 find max()
+	public static int max(int[] m) {
+		int max = m[0];
+		for(int i = 1; i < m.length; i = i + 1) {
+			if (max < m[i]) max = m[i];
+		}
+		return max;
+	}
+
+	public static void main(String[] args) {
+		int[] numbers = new int[]{9, 2, 15, 2, 22, 10, 6};
+		int maxNumber = max(numbers);
+		System.out.println(maxNumber);
+	/*	
+		int SIZE = Integer.parseInt(args[0]);
+		DrawTriangle(SIZE);
+	*/
 	}
 }
